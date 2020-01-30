8559b328de7d9741812188c2fcfbea94a4a315a3
cfowler650 <cfowler650@gmail.com>
2020-01-30 14:14:33 -0800

    connect mongodb atlas, get single citizen route working, and create citizen working

--- controllers/citizen.controller.js
+++ controllers/citizen.controller.js
@@ -14,21 +14,35 @@ exports.create = (req, res) => {
     location: req.body.location
   });
 
-  //save the new citizen - still need to set up DB
+  //save the new citizen
 
-  // citizen.save()
-  //   .then(data => {
-  //     res.send(data);
-  //   }).catch(err => {
-  //     res.status(500).send({
-  //       message: err.message || "Some error occurred while creating the Citizen."
-  //     });
-  //   });
-
-  //sends body back for testing
-
-  res.send(req.body);
-
+  citizen.save()
+    .then(data => {
+      res.send(data);
+    }).catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Citizen."
+      });
+    });
 };
 
-
+exports.findOne = (req, res) => {
+  Citizen.findById(req.params.citizenId)
+    .then(citizen => {
+      if (!citizen) {
+        return res.status(404).send({
+          message: "Citizen not found with id " + req.params.citizenId
+        });
+      }
+      res.send(citizen);
+    }).catch(err => {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({
+          message: "Citizen not found with id " + req.params.citizenId
+        });
+      }
+      return res.status(500).send({
+        message: "Error retrieving citizen with id " + req.params.citizenId
+      });
+    });
+};
--- routes/citizen.routes.js
+++ routes/citizen.routes.js
@@ -1,5 +1,8 @@
 module.exports = (app) => {
   const citizens = require('../controllers/citizen.controller.js');
-  // Create a new Player
+
+  // Create a new citizen
   app.post('/citizens', citizens.create);
+  // Get a new citizen
+  app.get('/citizens/:citizenId', citizens.findOne);
 }
