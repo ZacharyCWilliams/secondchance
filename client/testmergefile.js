b761539e72dffb691bff7ec3fe02b8a2a222e20f
zach <zachcwilliams@icloud.com>
2020-02-04 13:41:38 -0800

    same line test

--- client/testmergefile.js
+++ client/testmergefile.js
@@ -2,14 +2,9 @@ console.log('test')
 
 console.log(
   'babasdgasdfasdfasdfas'
-<<<<<<< HEAD
 );
 
-console.log('test comment');
-||||||| merged common ancestors
-);
-=======
+console.log("comment");
 );
 
 console.log("thepenpal")
->>>>>>> 3982365abc39e35c130d8fb916113f60e83e9304
