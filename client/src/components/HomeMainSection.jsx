7e265d7700a4407a0af8eb9035d0e82cfa08eb95
zach <zachcwilliams@icloud.com>
2020-02-04 17:07:19 -0800

    add location search icon

--- client/src/components/HomeMainSection.jsx
+++ client/src/components/HomeMainSection.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "../styles/Home/HomeMainSection.css";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 function HomeMainSection() {
+
   return (
     <section className="home-text-section">
       <h1 className="home-h1">
@@ -12,8 +14,26 @@ function HomeMainSection() {
         ThePenPal, your go to place for messaging inmates
       </p>
       <form className="home-form">
-        <input placeholder="Enter State.." className="search-bar" type="text" />
-        <input className="home-submit-button" type="submit" value=">" />
+        <div className="search-container">
+          <div className="location-icon">
+            <LocationOnIcon />
+          </div>
+          <div>
+            <input
+              placeholder="Enter State..."
+              className="search-bar"
+              type="text"
+            />
+          </div>
+          <div>
+            <button className="home-submit-button">
+              {" "}
+              <ArrowForwardIosIcon style={{ fontSize: "14px" }}>
+                ArrowForwardIosIcon{" "}
+              </ArrowForwardIosIcon>
+            </button>
+          </div>
+        </div>
       </form>
       <div className="button-flex-container">
         <button className="browse-all-button">Browse all states</button>
