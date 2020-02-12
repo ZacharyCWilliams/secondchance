1d603b4ae8870a4f7550979a42b499a5b118c631
zach <zachcwilliams@icloud.com>
2020-02-12 14:22:58 -0800

    add white modal overlay

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import ModalOverlay from "./ModalOverlay"
 
 const Modal = (props) => {
   return (
-    <div className="modal">
-      <div className="modal-header-section">
-        <div className="modal-exit-button">
-          <button style={{background: 'red'}} onClick={props.onClick}>X</button>
+    <div>
+      <ModalOverlay />
+      <div className="modal">
+        <div className="modal-header-section">
+          <div className="modal-exit-button">
+            <button style={{background: 'red'}} onClick={props.onClick}>X</button>
+          </div>
+          <h1 className="modal-title">Sign Up</h1>
         </div>
-        <h1 className="modal-title">Sign Up</h1>
-      </div>
-      <div className="modal-form-container">
-        <form className="modal-form">
-          <input type="text" className="modal-form-input" placeholder="username" />
-          <input type="text" className="modal-form-input" placeholder="email address" />
-          <input type="text" className="modal-form-input" placeholder="password" />
-        </form>
+        <div className="modal-form-container">
+          <form className="modal-form">
+            <input type="text" className="modal-form-input" placeholder="username" />
+            <input type="text" className="modal-form-input" placeholder="email address" />
+            <input type="text" className="modal-form-input" placeholder="password" />
+          </form>
+        </div>
       </div>
     </div>
   )
